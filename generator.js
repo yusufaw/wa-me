781e48127c86010d3cf589232f6fea6af72e040d
Yusuf Aji Wibowo <yusufaw@proton.me>
2022-11-19 22:28:23 +0800

    Update generator.js

--- generator.js
+++ generator.js
@@ -1,10 +1,10 @@
 var telepon = require("@namchee/telepon")
-const { Telegraf } = require('telegraf')
+const { Telegraf } = require('telegraf');
 require('dotenv').config();
 
 const bot = new Telegraf(process.env.MBOT_TOKEN)
 
-bot.on('text', ctx => {
+bot.on("text", ctx => {
     console.log(ctx.message.text);
     const inputPhoneNumber = telepon.parse(ctx.message.text);
     const formattedPhoneNumber = telepon.format(inputPhoneNumber, telepon.Standard.E164)
