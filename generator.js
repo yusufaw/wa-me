e7f2e5a0ae8c89317d40f81a3ff2e899857b3b5b
Yusuf Aji Wibowo <yusufaw@proton.me>
2022-11-20 19:26:18 +0800

    Update format message

--- generator.js
+++ generator.js
@@ -4,12 +4,19 @@ require('dotenv').config();
 
 const bot = new Telegraf(process.env.MBOT_TOKEN)
 
-bot.on("text", ctx => {
+bot.command('wame', ctx => {
     console.log(ctx.message.text);
-    const inputPhoneNumber = telepon.parse(ctx.message.text);
-    const formattedPhoneNumber = telepon.format(inputPhoneNumber, telepon.Standard.E164)
-    const wameFormat = `https://wa.me/${formattedPhoneNumber}`
-    ctx.reply(wameFormat)
+    try {
+        const inputPhoneNumber = telepon.parse(ctx.message.text);
+        const formattedPhoneNumber = telepon.format(inputPhoneNumber, telepon.Standard.E164)
+        const wameFormat = `https://wa.me/${formattedPhoneNumber}`
+        ctx.reply(wameFormat, {
+            parse_mode: "HTML",
+            disable_web_page_preview: true,
+        })
+    } catch (error) {
+        ctx.reply("Nomor telepon tidak sesuai");
+    }
 })
 
 bot.launch()
