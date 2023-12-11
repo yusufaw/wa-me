4e28f16226cbd5ce5384b060d74e17d91369fbc8
Yusuf Aji Wibowo <yusuf.wibowo@mekari.com>
2023-12-11 20:34:48 +0700

    Support NL code

--- generator.js
+++ generator.js
@@ -17,13 +17,22 @@ bot.command('wame', ctx => {
 function generateWaMeUrl(ctx) {
     console.log(ctx.message.text);
     try {
-        const inputPhoneNumber = telepon.parse(ctx.message.text);
-        const formattedPhoneNumber = telepon.format(inputPhoneNumber, telepon.Standard.E164)
-        const wameFormat = `https://wa.me/${formattedPhoneNumber}`
-        ctx.reply(wameFormat, {
-            parse_mode: "HTML",
-            disable_web_page_preview: true,
-        })
+        let input = ctx.message.text.replace(/[^\d\-\+]/g, '');
+        if (input.replace().startsWith('+31')) {
+            const wameFormat = `https://wa.me/${input}`
+            ctx.reply(wameFormat, {
+                parse_mode: "HTML",
+                disable_web_page_preview: true,
+            })
+        } else {
+            const inputPhoneNumber = telepon.parse(input);
+            const formattedPhoneNumber = telepon.format(inputPhoneNumber, telepon.Standard.E164);
+            const wameFormat = `https://wa.me/${formattedPhoneNumber}`
+            ctx.reply(wameFormat, {
+                parse_mode: "HTML",
+                disable_web_page_preview: true,
+            })
+        }
     } catch (error) {
         ctx.reply("Nomor telepon tidak sesuai");
     }
